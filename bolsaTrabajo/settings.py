9a2a2311ad5356f8dcb1804e34db46bcea4a361a
Angel Jaret Lopez Hernandez <jaretguetta@icloud.com>
2025-06-19 01:49:02 -0600

    Allow host

--- bolsaTrabajo/settings.py
+++ bolsaTrabajo/settings.py
@@ -20,6 +20,7 @@ pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ALLOWED_HOSTS = ['*']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
